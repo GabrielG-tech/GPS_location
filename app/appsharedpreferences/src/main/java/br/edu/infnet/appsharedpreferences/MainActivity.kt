89b6349f32446a16b7d1df212f489933c89d4bf8
gabriel.gsouza <gabriel@mpsouza.com.br>
2022-10-24 12:02:35 -0300

    SharedPreferences_1.0

---
+++ app/appsharedpreferences/src/main/java/br/edu/infnet/appsharedpreferences/MainActivity.kt
@@ -0,0 +1,66 @@
+package br.edu.infnet.appsharedpreferences
+
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var seekBarSound: SeekBar? = null
+    private var seekBarBrightness: SeekBar? = null
+
+    private var radioGroupDiffLevel: RadioGroup? = null
+    private var radioButtonEasy: RadioButton? = null
+    private var radioButtonMedium: RadioButton? = null
+    private var radioButtonHard: RadioButton? = null
+
+    private var buttonSave: Button? = null
+
+    private var edtPersonName: EditText? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        seekBarBrightness = findViewById<View>(R.id.seekBar_brightness) as SeekBar
+        seekBarSound = findViewById<View>(R.id.seekBar_sound) as SeekBar
+        seekBarBrightness!!.max = 100
+        seekBarSound!!.max = 100
+
+        radioGroupDiffLevel = findViewById<View>(R.id.radioGroup_diffLevel) as RadioGroup
+        radioButtonEasy = findViewById<View>(R.id.radioButton_easy) as RadioButton
+        radioButtonMedium = findViewById<View>(R.id.radioButton_medium) as RadioButton
+        radioButtonHard = findViewById<View>(R.id.radioButton_hard) as RadioButton
+
+        edtPersonName = findViewById<View>(R.id.editTextTextPersonName) as EditText
+
+        buttonSave = findViewById<View>(R.id.button_save) as Button
+
+        buttonSave!!.setOnClickListener { view -> doSave(view) }
+
+        loadGameSetting()
+
+    }
+
+    private fun loadGameSetting() {
+        val sharedPreferences = getSharedPreferences("gameSetting", MODE_PRIVATE)
+        if (sharedPreferences != null) {
+            val brightness = sharedPreferences.getInt("brightness", 90)
+            val sound = sharedPreferences.getInt("sound", 95)
+            val edtPersonName = sharedPreferences.getString("text", "Name")
+            val checkedRadioButtonId =
+                sharedPreferences.getInt("checkedRadioButtonId", R.id.radioButton_medium)
+            seekBarSound!!.progress = sound
+            seekBarBrightness!!.progress = brightness
+            radioGroupDiffLevel!!.check(checkedRadioButtonId)
+        } else {
+            radioGroupDiffLevel!!.check(R.id.radioButton_medium)
+            Toast.makeText(this, "Use the default game setting", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun doSave(view: View?) {
+
+    }
+}
